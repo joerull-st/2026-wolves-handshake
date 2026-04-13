8ef9cc551df2dd515926753405a5a40e71913b5d
joerull-st <joe.rull@startribune.com>
2026-04-13 10:17:03 -0500

    kssdg

--- src/components/Hero/Hero.svelte
+++ src/components/Hero/Hero.svelte
@@ -63,9 +63,9 @@ Renders a visual replica of the Immersive Template hero.
 
     <GridRow variant={"fullBleed"}>
       <div class="relative flex items-center justify-center h-screen mb-4 overflow-hidden bg-[white] xl:max-h-[1000px] lg:max-h-[900px] md:max-h-[800px] xs:max-h-[700px]">
-        <div class="absolute left-8 top-8 xs:left-2 xs:top-2 z-30 xl:p-6 lg:p-4 md:p-4 xs:p-2 text-2xl !text-[white] text-left font-editorial-news-heading-01 max-w-[700px] mt-auto drop-shadow-md">
+        <div class="absolute left-8 bottom-4 xs:left-2 xs:top-2 z-30 xl:p-6 lg:p-4 md:p-4 xs:p-2 text-2xl !text-[white] text-left font-editorial-news-heading-01 max-w-[700px] mt-auto drop-shadow-md">
 
-            <h1 class="xl:text-[4rem] lg:text-[4rem] md:text-[3rem] xs:text-[2rem]">Timberwolves guard Mike Conley is a man of 1,000 handshakes. Well, close.</h1>
+            <h1 class="xl:text-[3.5rem] lg:text-[3rem] md:text-[2rem] xs:text-[2rem]">Timberwolves guard Mike Conley is a man of 1,000 handshakes. Well, close.</h1>
         </div>
         <video autoplay="" loop="" muted="true" class="object-cover absolute h-full w-full">
             <source src="https://static.startribune.com/news/projects/all/2026-WOLVES-HANDSHAKE/video%20assets/HANDSHAKES_HERO_16X9%20%281%29.mp4" type="video/mp4">Your browser does not support the video tag.
